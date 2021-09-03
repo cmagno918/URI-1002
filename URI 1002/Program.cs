060c5b0eab228dce218b5c478dae5d3d453f41f7
cmagno918@gmail.com <cmagno918@gmail.com>
2021-09-03 01:55:48 -0300

    Initial commit

---
+++ URI 1002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace URI_1002
+{
+    class Program
+     
+    {
+        static void Main(string[] args)
+        {
+
+            double R, A, pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = pi * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
